6871063a804310a03f91b7d55140d89c0a792a9f
Magnus Lind <magli518@student.liu.se>
2019-11-21 12:38:11 +0100

    Finished task1

--- task1.cpp
+++ task1.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+int string_lenght(const char * s)
+{
+    int size;
+    for(size = 0; *(s+size) != '\0'; size++);
+    return size;
+}
+
+void print_lenght(const char* s)
+{
+    std::cout << "Length of " << s << " is: " << string_lenght(s) << std::endl;
+}
+
 int main(void)
 {
+    print_lenght("hello");
+    print_lenght("there");
+    print_lenght("is");
+    print_lenght("it");
+    print_lenght("you");
+    print_lenght("lkösadjflkj");
+
     return 0;
 }
